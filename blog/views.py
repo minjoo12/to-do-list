7611c294a362f9d7c2ca0e104fc62fddb556be18
minjoo <hope_0726@hanmail.net>
2021-01-29 13:06:22 +0900

    new version!

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Todo
 from .forms import TodoForm
 
@@ -6,6 +7,7 @@ def todo_list(request):
     todos = Todo.objects.all() # 쿼리셋으로 불러오기
     return render(request, 'blog/todo_list.html', {'todos': todos})
 
+@login_required
 def todo_new(request):
     if request.method == "POST":
         form = TodoForm(request.POST)
@@ -22,6 +24,7 @@ def todo_detail(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     return render(request, 'blog/todo_detail.html', {'todo': todo})
 
+@login_required
 def todo_edit(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     if request.method == "POST":
@@ -35,6 +38,7 @@ def todo_edit(request, pk):
         form = TodoForm(instance=todo)
     return render(request, 'blog/todo_edit.html', {'form': form})
 
+@login_required
 def todo_remove(request, pk):
     todo = get_object_or_404(Todo, pk=pk)
     todo.delete()
